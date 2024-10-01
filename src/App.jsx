8017cfcba5508d68a021100ebb270b8a241c414d
iuripejara <iuripp91@gmail.com>
2024-10-01 16:27:09 +0100

    01/10/2024

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+
+  return (
+    <>
+      <Header/>
+      <div className='dark:bg-zinc-800 text-white'>
+          <h1>oie</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Avatar.jsx
@@ -0,0 +1,10 @@
+export default function Avatar (){
+    return(
+        <img
+            className="rounded-full border-2 border-zinc-200"
+            width={48}
+            height={48}
+            src="/user.svg"
+            alt="avatar" />
+    )
+}
